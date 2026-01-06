11af6e63deead88f97712b83e540209726c9fd7c
iyann1255 <tessbarca1981@gmail.com>
2026-01-07 00:19:05 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,11 @@ load_dotenv()
 API_ID = int(os.getenv("API_ID", "0"))
 API_HASH = os.getenv("API_HASH", "").strip()
 
+MONGO_URL = os.getenv("MONGO_URL", "").strip()
+MONGO_DB = os.getenv("MONGO_DB", "chatrep").strip()
+
 if not API_ID or not API_HASH:
     raise SystemExit("API_ID / API_HASH belum diset di .env")
+
+if not MONGO_URL:
+    raise SystemExit("MONGO_URL belum diset di .env")
