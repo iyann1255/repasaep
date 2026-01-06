9cd88e4a90820660a31fdb5a8262d1e851fe4e27
iyann1255 <tessbarca1981@gmail.com>
2026-01-06 23:42:29 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_ID = int(os.getenv("API_ID", "0"))
-API_HASH = os.getenv("API_HASH", "")
-SESSION = os.getenv("SESSION", "").strip()
+API_HASH = os.getenv("API_HASH", "").strip()
 
 if not API_ID or not API_HASH:
     raise SystemExit("API_ID / API_HASH belum diset di .env")
